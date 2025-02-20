4935f178dc384fccb99aa9efe1d48820bb53cbe5
MGoyal1903 <mayankgoyal19032000@gmail.com>
2025-02-20 19:00:38 +0530

    Day-2

---
+++ Day-2/Variables.py
@@ -0,0 +1,26 @@
+'''
+Create the following variables of different types and print them.
+name: String
+age: Integer
+height: Floating-point number
+is_student: Boolean
+email: None (representing a null value)
+siblings: List
+address: Dictionary containing street and city
+'''
+
+name = "MG"
+age = 24
+height = 170.18
+is_student = True
+email = None
+siblings = ["Elder Sister, Younger Brother"]
+address = {
+    'street' : 'ghjhg',
+    'city' : 'hgfh',
+    'HNo.' : '874224',
+    'state' : 'jhdfho',
+    'country' : 'India'
+}
+
+print(f"Name: {name}\n Age: {age}\n Height: {height}\n Student: {is_student}\n Email : {email}\n Siblings: {siblings}\n Address: {address}")
